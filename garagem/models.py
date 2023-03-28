04de6019a768a59265d7ce9a85e74decd52e5082
Vencce <Vitorferreira20005@gmail.com>
2023-03-28 20:39:54 -0300

    Criação modelo marca

--- garagem/models.py
+++ garagem/models.py
@@ -12,3 +12,11 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome.bold
+
