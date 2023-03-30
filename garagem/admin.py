e04f017719d65bc822e0568638038ae1a1574c61
Vencce <Vitorferreira20005@gmail.com>
2023-03-30 20:18:39 -0300

    Finalização do projeto

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from garagem.models import Categoria, Marca, Acessorio, Cor
+from garagem.models import Categoria, Marca, Acessorio, Cor, Veiculo
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
 admin.site.register(Acessorio)
 admin.site.register(Cor)
+admin.site.register(Veiculo)
--- garagem/models.py
+++ garagem/models.py
@@ -24,4 +24,12 @@ class Cor(models.Model):
     def _str_(self):
         return self.descricao
 
-
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="Veículos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="Veículos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="Veículos")
+    ano = models.IntegerField(null=True, blank=True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0)
+    
+    def _str_(self):
+        return f"{self.marca}, {self.categoria}, {self.ano}, {self.cor}"
