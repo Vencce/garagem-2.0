9f9eb19bbeb8d0a156188c022f2120e84524fa21
Vencce <Vitorferreira20005@gmail.com>
2023-03-30 20:16:40 -0300

    cor, acessorio

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from garagem.models import Categoria, Marca
+from garagem.models import Categoria, Marca, Acessorio, Cor
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Acessorio)
+admin.site.register(Cor)
