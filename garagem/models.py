e645c3ee752838a1e67919f6e10b86ac76554322
Vencce <Vitorferreira20005@gmail.com>
2023-03-31 16:27:37 -0300

    Finalizar

--- garagem/models.py
+++ garagem/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True, blank=True)
@@ -17,6 +18,9 @@ class Acessorio(models.Model):
     
     def __str__(self):
         return self.descricao
+
+    class Meta:
+        verbose_name_plural = "Acessórios"
     
 class Cor(models.Model):
     descricao = models.CharField(max_length=100)
@@ -25,14 +29,17 @@ class Cor(models.Model):
         return self.descricao
 
     class Meta:
-        verbose_names_plural = "Cores"
+        verbose_name_plural = "Cores"
 
 class Veiculo(models.Model):
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="Veículos")
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="Veículos")
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="Veículos")
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(null=True, blank=True, default=0)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     
     def __str__(self):
         return f"{self.marca} {self.categoria} {self.ano} {self.cor}"
+
+    class Meta:
+        verbose_name_plural = "Veículos"
